fd39e8de08ec6ec80bcda3688c1ac82a4e3c7f70
Dave Strus <dave@davestrus.com>
2014-12-14 12:20:21 -0500

    Add the option to use a secret agent name.

--- js/questly.js
+++ js/questly.js
@@ -1,7 +1,8 @@
 function questly() {
-  var myForm, nameField, questField, colorField, userData;
+  var myForm, nameField, secretNameField, questField, colorField, userData;
   myForm = document.querySelector('form');
   nameField = myForm.elements.name;
+  secretNameField = myForm.elements.secret_name;
   questField = myForm.elements.quest;
   colorField = myForm.elements.color;
 
@@ -9,9 +10,11 @@ function questly() {
     ev.preventDefault();
     userData = {
       name: nameField.value,
+      secretName: secretNameField.value,
       quest: questField.value,
       color: colorField.value,
-      friend: this.elements.friend.checked
+      friend: this.elements.friend.checked,
+      useSecret: this.elements.use_secret.checked
     }
     writeData();
   }
@@ -22,10 +25,43 @@ function questly() {
     if (userData.friend) {
       friendStory = 'Hey, I see you brought your friend!';
     }
-    storyDiv.innerHTML = userData.name + ', ' + userData.quest +
+    var nameToUseInStory = userData.name;
+    if (userData.useSecret) {
+      nameToUseInStory = userData.secretName;
+    }
+    var story = nameToUseInStory + ', ' + userData.quest +
     '. Don\'t forget your ' + userData.color + ' raincoat. ' + friendStory;
+
+    storyDiv.innerHTML = story;
   }
 
   myForm.onsubmit = getValues;
 }
 questly();
+
+
+
+
+
+
+
+
+
+// LAB ===================================================================
+//
+// * Move the story above the form.
+// * Add a second text field below name, named 'secret_agent_name'.
+// * Add a checkbox to allow the user to specify whether to use the secret
+//   agent name.
+// * Change the story to use the secret agent name *if* the checkbox was checked.
+
+
+
+
+
+
+
+
+
+
+//.
