58885ede4972ef18ffc1cdbd98c1bb27747ea731
Dave Strus <dave@davestrus.com>
2014-12-13 20:45:53 -0500

    Track experiments.

---
+++ js/experiments.js
@@ -0,0 +1,22 @@
+function eat(food) {
+  return food + ' tasted good';
+}
+
+// Option 1
+console.log(eat('bananas'));
+
+// Option 2
+// var output = eat('bananas');
+// console.log(output);
+
+
+
+
+// function document.write(str)
+// Writes the contents of `str` to the current HTML document
+document.write('<h1>My Brain Hurts</h1>');
+
+
+// function eat(food)
+// Tells you that the food is good.
+eat('bananas');
