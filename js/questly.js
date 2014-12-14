1c0b4d96df49e6b35e9a32a99344c1cf25f1d844
Dave Strus <dave@davestrus.com>
2014-12-14 11:25:34 -0500

    Add the friend checkbox.

--- js/questly.js
+++ js/questly.js
@@ -1,23 +1,31 @@
-var myForm, nameField, questField, colorField, userData;
-myForm = document.querySelector('form');
-nameField = myForm.elements.name;
-questField = myForm.elements.quest;
-colorField = myForm.elements.color;
+function questly() {
+  var myForm, nameField, questField, colorField, userData;
+  myForm = document.querySelector('form');
+  nameField = myForm.elements.name;
+  questField = myForm.elements.quest;
+  colorField = myForm.elements.color;
 
-function getValues(ev) {
-  ev.preventDefault();
-  userData = {
-    name: nameField.value,
-    quest: questField.value,
-    color: colorField.value
+  function getValues(ev) {
+    ev.preventDefault();
+    userData = {
+      name: nameField.value,
+      quest: questField.value,
+      color: colorField.value,
+      friend: this.elements.friend.checked
+    }
+    writeData();
   }
-  writeData();
-}
 
-function writeData() {
-  var storyDiv = document.getElementById('story');
-  storyDiv.innerHTML = userData.name + ', ' + userData.quest +
-  '. Don\'t forget your ' + userData.color + ' raincoat.';
-}
+  function writeData() {
+    var storyDiv = document.getElementById('story');
+    var friendStory = '';
+    if (userData.friend) {
+      friendStory = 'Hey, I see you brought your friend!';
+    }
+    storyDiv.innerHTML = userData.name + ', ' + userData.quest +
+    '. Don\'t forget your ' + userData.color + ' raincoat. ' + friendStory;
+  }
 
-myForm.onsubmit = getValues;
+  myForm.onsubmit = getValues;
+}
+questly();
