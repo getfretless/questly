6a2e78f717ae15fde2538ac11be42a19a0b642d5
Dave Strus <dave@davestrus.com>
2014-12-14 10:15:30 -0500

    Change quest to a radio button.

--- js/questly.js
+++ js/questly.js
@@ -1,20 +1,23 @@
-var myForm, nameField, questField, userData;
+var myForm, nameField, questField, colorField, userData;
 myForm = document.querySelector('form');
 nameField = myForm.elements.name;
 questField = myForm.elements.quest;
+colorField = myForm.elements.color;
 
 function getValues(ev) {
   ev.preventDefault();
   userData = {
     name: nameField.value,
-    quest: questField.value
+    quest: questField.value,
+    color: colorField.value
   }
   writeData();
 }
 
 function writeData() {
   var storyDiv = document.getElementById('story');
-  storyDiv.innerHTML = userData.name + ', ' + userData.quest;
+  storyDiv.innerHTML = userData.name + ', ' + userData.quest +
+  '. Don\'t forget your ' + userData.color + ' raincoat.';
 }
 
 myForm.onsubmit = getValues;
