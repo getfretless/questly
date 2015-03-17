9d1780017f70537efcc05b14b8a41a192c46e452
David Jones <unixmonkey1@gmail.com>
2015-03-17 12:06:16 -0400

    Updates from mar2015 cohort

---
+++ js/first.js
@@ -0,0 +1,40 @@
+console.log('I may not heart JavaScript yet, but with the help of my new Dave friends, I will learn to.');
+// var name = prompt('What is your name?');
+// var quest = prompt('What is your quest?');
+// document.write('This isn\'t annoying at all, ' + name + '.' + '<br><br>Really, you should already be ' + quest);
+
+// Write an H1 element to the page that will read as follows:
+// C:\Windows\system32
+document.write('<H1> C:\\Windows\\system32 </H1>')
+
+
+// var the_other_number = 2;
+
+// function triple(n) {
+//  // debugger;
+//  return n * the_other_number;
+// }
+
+// var number_of_students = 6;
+// document.writeln(triple(number_of_students, 3) + '<br>');
+// alert(the_other_number);
+
+// function add_to_list(thing_to_add) {
+//  list.push(thing_to_add);
+// }
+
+// var list = [];
+
+// add_to_list('bread');
+// alert(list)
+
+// function addRedBorder(ev) {
+//  this.style.border = '2px solid red';
+// }
+
+
+// var first_input = document.querySelector('input');
+// addRedBorder.call(first_input);
+
+// var my_div = document.querySelector('div');
+// my_div.onclick = addRedBorder;
--- js/questly.js
+++ js/questly.js
@@ -1,67 +1,55 @@
 function questly() {
-  var myForm, nameField, secretNameField, questField, colorField, userData;
-  myForm = document.querySelector('form');
-  nameField = myForm.elements.name;
-  secretNameField = myForm.elements.secret_name;
-  questField = myForm.elements.quest;
-  colorField = myForm.elements.color;
 
-  function getValues(ev) {
+  function uncheckable(element) {
+    var checkable_types = ['radio', 'checkbox'];    
+    return checkable_types.indexOf(element.type) === -1;
+  }
+
+  function getFormValues(ev) {
+    // Prevent the default submit behavior.
     ev.preventDefault();
-    userData = {
-      name: nameField.value,
-      secretName: secretNameField.value,
-      quest: questField.value,
-      color: colorField.value,
-      friend: this.elements.friend.checked,
-      useSecret: this.elements.use_secret.checked
+    
+    // Save the values of each form field into variables
+    var the_form;
+    
+    the_form = ev.currentTarget;
+    var form_values = {};
+
+    for (var i=0; i < the_form.elements.length; i++) {
+      var current_element = the_form.elements[i];
+      
+      if (current_element.checked || uncheckable(current_element)) {
+        form_values[current_element.name] = current_element.value;
+      }
     }
-    writeData();
+
+    writeStory(form_values);
   }
 
-  function writeData() {
-    var storyDiv = document.getElementById('story');
-    var friendStory = '';
-    if (userData.friend) {
-      friendStory = 'Hey, I see you brought your friend!';
-    }
-    var nameToUseInStory = userData.name;
-    if (userData.useSecret) {
-      nameToUseInStory = userData.secretName;
-    }
-    var story = nameToUseInStory + ', ' + userData.quest +
-    '. Don\'t forget your ' + userData.color + ' raincoat. ' + friendStory;
+  function writeStory(form_values) {
+    var paragraph = document.getElementById('story');
+    var quest_items = ['walking stick', 'bag of holding', 'george michael CD', 'towel', 12];
+    var name_to_print = form_values.name;
 
-    storyDiv.innerHTML = story;
+    if (form_values.use_secret) {
+      name_to_print = form_values.secret_agent_name;
+    }
+
+    paragraph.innerText = name_to_print + ', go forth and complete your quest: ' + 
+      form_values.quest + '. Don\'t forget your ' + form_values.color + ' raincoat!';
+
+    paragraph.innerText += ' You should probably also bring these items: ';
+
+    var li_string = '';
+    for (var n = 0; n < quest_items.length; n++) {
+      li_string += '<li>' + quest_items[n] + '</li>';
+    }
+
+    paragraph.innerHTML += '<ul>' + li_string + '</ul>';
   }
 
-  myForm.onsubmit = getValues;
+  var form = document.querySelector('form');
+  form.onsubmit = getFormValues;
 }
+
 questly();
-
-
-
-
-
-
-
-
-
-// LAB ===================================================================
-//
-// * Move the story above the form.
-// * Add a second text field below name, named 'secret_agent_name'.
-// * Add a checkbox to allow the user to specify whether to use the secret
-//   agent name.
-// * Change the story to use the secret agent name *if* the checkbox was checked.
-
-
-
-
-
-
-
-
-
-
-//.
